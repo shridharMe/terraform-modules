a046ec7976d1e2df077e6d40d9a5396861a59e82
shridhar01_patil <shridhar.patil@accenture.com>
2018-08-16 00:11:30 +0100

    fixed the parameter type

--- modules/eks/eks-cluster/interface.tf
+++ modules/eks/eks-cluster/interface.tf
@@ -1,7 +1,9 @@
 variable "vpc_id" {}
 variable "public_subnets" {}
 
-variable "private_subnets_cidr" {}
+variable "private_subnets_cidr" {
+    type = "list"
+}
 
 variable "cluster-name" {}
 variable "workstation-external-cidr" {}
--- modules/eks/eks-worker-node/interface.tf
+++ modules/eks/eks-worker-node/interface.tf
@@ -1,6 +1,8 @@
 variable "vpc_id" {}
 variable "private_subnet" {}
-variable "public_subnet_cidr" {}
+variable "public_subnet_cidr" {
+    type = "list"
+}
 variable "node-instance-type" {}
 variable "desired-capacity" {}
 variable "max-size" {}
