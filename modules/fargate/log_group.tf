4b57c7e7af73e3cee35c94ab4b70b6b3d8b1fb05
shridhar01_patil <shridhar.patil@accenture.com>
2018-08-16 19:37:47 +0100

    moved out contianer defination

--- modules/fargate/log_group.tf
+++
@@ -1,5 +0,0 @@
-resource "aws_cloudwatch_log_group" "main" {
-  name              = "${var.name_prefix}"
-  retention_in_days = "${var.log_retention_in_days}"
-  tags              = "${var.tags}"
-}
--- modules/fargate/task.tf
+++ modules/fargate/task.tf
@@ -1,4 +1,4 @@
-data "aws_region" "current" {}
+
 
 resource "aws_ecs_task_definition" "task" {
   family                   = "${var.name_prefix}"
@@ -8,28 +8,5 @@ resource "aws_ecs_task_definition" "task" {
   cpu                      = "${var.task_definition_cpu}"
   memory                   = "${var.task_definition_memory}"
   task_role_arn            = "${aws_iam_role.task.arn}"
-
-  container_definitions = <<EOF
-[{
-     "cpu": ${var.task_definition_cpu},
-      "image": "${var.image}:${var.image_version}",
-      "memory": ${var.task_definition_memory},
-      "name": "${var.environment}-${var.name_prefix}",    
-      "networkMode": "awsvpc",
-      "logConfiguration": {
-          "logDriver": "awslogs",
-          "options": {
-            "awslogs-group": "${aws_cloudwatch_log_group.name}",
-            "awslogs-region": "${aws_region.current.name}",
-            "awslogs-stream-prefix": "ecs"
-          }
-      },    
-      "portMappings": [
-        {
-          "containerPort": ${var.container_port},
-          "hostPort": ${var.host_port}
-        }
-      ]
-}]
-EOF
+  container_definitions    = "${var.container_definitions}
 }
