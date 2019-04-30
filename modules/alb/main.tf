a8585af03611d8aa2f2b58297be067ed487a3f80
Shridhar <36041359+shridharMe@users.noreply.github.com>
2019-04-30 10:37:39 +0100

    added 2 new alb variables

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,11 +1,11 @@
 resource "aws_alb" "alb" {
-  name            = "${var.name_prefix}"
+  name            = "${var.dns_name}"
   internal        = "${var.internal}"
   security_groups = ["${var.security_groups}"]
   subnets         = ["${var.subnets}"]
 
   tags {
-    Name        = "${var.name_prefix}"
+    Name        = "${var.recordset_name}"
     Environment = "${var.environment}"
     Terraform   = "${var.terraform}"
     Owner       = "${var.owner}"
@@ -15,7 +15,7 @@ resource "aws_alb" "alb" {
 
 resource "aws_route53_record" "webapp_ext_fqdn" {
   zone_id = "${var.route53zoneid}"
-  name    = "${aws_alb.alb.name}"
+  name    = "${var.recordset_name}"
   type    = "${var.route53type}"
   ttl     = "${var.route53ttl}"
   records = ["${aws_alb.alb.dns_name}"]
